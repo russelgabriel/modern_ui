4d88b2c992c19eff0359d281362c594d98db262c
russelsy <russelgabrielsy@gmail.com>
2023-10-16 17:02:42 -0400

    fix FeedbackCard import bug

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,6 @@
 import { feedback } from '../constants'
 import styles from '../style'
-import FeedbackCard from './FeedBackCard'
+import FeedbackCard from './FeedbackCard'
 
 const Testimonials = () => (
   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
